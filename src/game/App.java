4815e0e67a7525cd37091891c43e75eafaa1c425
Riccardo <riccardo.nicolini@outlook.com>
2022-10-01 17:48:13 +0200

    gitignore

--- src/game/App.java
+++ src/game/App.java
@@ -1,13 +1,9 @@
 package game;
 
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
  
