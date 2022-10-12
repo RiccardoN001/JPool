5d0830e4cb5b60b29de4be30102aceaa5d8ef2da
Giovanni <giovanniversiglioni@outlook.it>
2022-10-12 23:14:13 +0200

    Added GameScene.fxml and Player, GameController classes

---
+++ src/game/GameController.java
@@ -0,0 +1,19 @@
+package game;
+
+import javafx.fxml.FXML;
+import javafx.scene.input.MouseDragEvent;
+import javafx.scene.input.MouseEvent;
+
+public class GameController {
+
+    @FXML
+    void Moveline(MouseEvent event) {
+
+    }
+
+    @FXML
+    void released(MouseEvent event) {
+
+    }
+
+}
